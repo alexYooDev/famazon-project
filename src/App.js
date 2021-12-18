d0f5a3ad6c0698359036b3d4a94eb4826536dba0
d_flex <hiyoo95@gmail.com>
2021-12-19 00:18:39 +0900

    implemented header component with scss styles applied to it _> linking with / dir

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import Shop from './pages/shop/shop.component';
 
 function App() {
   return (
-    <Routes>
-      <Route exact path='/' element={<HomePage />} />
-      <Route exact path='/shop' element={<Shop />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+        <Route exact path='/shop' element={<Shop />} />
+      </Routes>
+    </>
   );
 }
 
