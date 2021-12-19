5a641ee974f4206434bbb39e3f0862033c72fc43
d_flex <hiyoo95@gmail.com>
2021-12-19 09:24:54 +0900

    sign-in-and-sign-up, sign-in, form-input components implemented with scss styles

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
+
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
-import Shop from './pages/shop/shop.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -12,7 +14,8 @@ function App() {
       <Header />
       <Routes>
         <Route exact path='/' element={<HomePage />} />
-        <Route exact path='/shop' element={<Shop />} />
+        <Route exact path='/shop' element={<ShopPage />} />
+        <Route exact path='/sign-in' element={<SignInAndSignUp />} />
       </Routes>
     </>
   );
