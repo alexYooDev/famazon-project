1e278eff33ecf9fbc1579bde4d8e3ea814ec3ef6
d_flex <hiyoo95@gmail.com>
2021-12-23 00:34:44 +0900

    updated shop data id and updated cart utils to group items in cart

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,8 +7,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className='preview'>
       {items
         .filter((item, idx) => idx < 4)
-        .map(({ id, price, ...otherItemProps }) => (
-          <CollectionItem key={id} price={`$${price}`} {...otherItemProps} />
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
