30a7eb3e97fe07b69c4cc47bbb1ebf35e1bdeaaf
d_flex <hiyoo95@gmail.com>
2021-12-26 18:44:41 +0900

    yarn removed

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -6,8 +6,6 @@ import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 import './collections-overview.styles.scss';
 
 const CollectionsOverview = ({ collections }) => {
-  console.log(collections);
-
   const CollectionPreview = React.lazy(() =>
     import('../../components/collection-preview/collection-preview.component')
   );
